5895a2fd6cc007b2797a04c2e332527aa7830247
Eyad156 <eyad1509@gmail.com>
2024-07-20 23:26:30 +0300

    @Eyad156

--- main.py
+++ main.py
@@ -2,42 +2,54 @@ import requests
 import socket
 import json
 import cowsay
-from colorama import Fore
+import random
+from colorama import Fore, Style, init
+
+# Initialize colorama
+init(autoreset=True)
+
+# List of colors
+colors = [Fore.RED, Fore.GREEN, Fore.YELLOW, Fore.BLUE, Fore.MAGENTA, Fore.CYAN, Fore.WHITE]
+
+# Function to get a random color
+def random_color():
+    return random.choice(colors)
+
 cowsay.daemon("Info-Gathering")
+
 def get_server_headers(domain):
     try:
-        
         response = requests.get(f'http://{domain}')
         if response.status_code == 200:
             server = response.headers.get('Server', 'Unknown')
-            print(f"Server Info: {server}")
+            print(random_color() + f"Server Info: {server}")
         else:
-            print(f"Failed to connect to {domain}")
+            print(random_color() + f"Failed to connect to {domain}")
     except requests.exceptions.RequestException as e:
-        print(f"An error occurred: {e}")
+        print(random_color() + f"An error occurred: {e}")
 
 def get_ip_info(ip):
     try:
         response = requests.get(f'https://ipinfo.io/{ip}/json')
         if response.status_code == 200:
             data = response.json()
-            print(f"IP Info: {json.dumps(data, indent=2)}")
+            print(random_color() + f"IP Info: {json.dumps(data, indent=2)}")
         else:
-            print("Failed to retrieve IP information")
+            print(random_color() + "Failed to retrieve IP information")
     except requests.exceptions.RequestException as e:
-        print(f"An error occurred: {e}")
+        print(random_color() + f"An error occurred: {e}")
 
 def main():
     domain = input("Enter website name (e.g., 'google.com') -> ")
     try:
         ip = socket.gethostbyname(domain)
-        print(f"IP Address: "+Fore.GREEN+f"{ip}")
+        print(random_color() + f"IP Address: {ip}")
         
-        get_server_headers(Fore.GREEN + domain)
-        get_ip_info(Fore.GREEN + ip)
+        get_server_headers(domain)
+        get_ip_info(ip)
         
     except socket.gaierror:
-        print(f"Could not resolve domain: {domain}")
+        print(random_color() + f"Could not resolve domain: {domain}")
 
 if __name__ == "__main__":
     main()
